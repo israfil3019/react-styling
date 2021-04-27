ee8da5e8220d7fe7652208739401b4d28da4c9f2
israfil <israfil3019@gmail.com>
2021-04-28 00:37:25 +0300

    add using of dot notation and bracket notation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 // import Inline from './components/inline/inline'
 // import StyleSheet from './components/stylesheets/styleSheet'
-import "./styles.css"
-import styles from "./styles.module.css"
-
+// import "./styles.css"
+// import styles from "./styles.module.css"
+import ModuleBtn from "./components/moduleCss/ModuleBtn"
 
 const App = () => {
 // console.log("styles obj: ", styles); */
@@ -10,8 +10,11 @@ const App = () => {
     <div>
       {/* <Inline />      */}
       {/* <StyleSheet primary={false} /> */}
-      <h1 className="error">Something went wrong</h1>
-      <h1 className={styles.success}>200 OK success </h1>
+      {/* <h1 className="error">Something went wrong</h1>
+      <h1 className={styles.success}>200 OK success </h1> */}
+      <h1>Module Css</h1>
+      <ModuleBtn btnClass="primary" btnName="Module" />
+      <ModuleBtn  btnClass="secondary" btnName="CSS" />
 
     </div>
   );
