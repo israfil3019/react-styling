e556d1d135119a0df6cddd0bc8847e7484e15f67
israfil <israfil3019@gmail.com>
2021-04-28 00:17:50 +0300

    add difference of module.css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 // import Inline from './components/inline/inline'
-import StyleSheet from './components/stylesheets/styleSheet'
+// import StyleSheet from './components/stylesheets/styleSheet'
+import "./styles.css"
+import styles from "./styles.module.css"
+
 
 const App = () => {
-
+// console.log("styles obj: ", styles); */
   return (
     <div>
       {/* <Inline />      */}
-      <StyleSheet primary={false} />
+      {/* <StyleSheet primary={false} /> */}
+      <h1 className="error">Something went wrong</h1>
+      <h1 className={styles.success}>200 OK success </h1>
 
     </div>
   );
